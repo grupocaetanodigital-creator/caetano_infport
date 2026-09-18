460595c5cc60da03c348789c602f1b125675bf8b
grupocaetanodigital-creator <grupocaetanodigital@gmail.com>
2026-09-18 00:37:14 -0300

    Register service worker in main.jsx
    Added service worker registration for PWA optimization.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { register as registerServiceWorker } from './registerServiceWorker';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Registra o Service Worker para otimização PWA e suporte na guarita
+registerServiceWorker();
