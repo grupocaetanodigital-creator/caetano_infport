9d32f5376ba97747728594ddbf4bbaf3af01f32f
grupocaetanodigital-creator <grupocaetanodigital@gmail.com>
2026-09-18 01:01:17 -0300

    Simplify service worker registration logic
    Removed NODE_ENV check for service worker registration and updated console log messages for clarity.

--- public/registerServiceWorker.js
+++ public/registerServiceWorker.js
@@ -1,13 +1,13 @@
 export function register() {
-  if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
         .register('/sw.js')
         .then((registration) => {
-          console.log('Service Worker do INFPORT 1.0 registrado com sucesso:', registration.scope);
+          console.log('Service Worker do INFPORT registrado com sucesso:', registration.scope);
         })
         .catch((error) => {
-          console.error('Falha ao registrar o Service Worker:', error);
+          console.error('Falha ao registrar Service Worker:', error);
         });
     });
   }
