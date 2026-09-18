753bce27a15e6bac46d05064330618bea3d1a08c
grupocaetanodigital-creator <grupocaetanodigital@gmail.com>
2026-09-18 00:36:38 -0300

    Add service worker registration and unregistration functions

---
+++ public/registerServiceWorker.js
@@ -0,0 +1,26 @@
+export function register() {
+  if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.js')
+        .then((registration) => {
+          console.log('Service Worker do INFPORT 1.0 registrado com sucesso:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Falha ao registrar o Service Worker:', error);
+        });
+    });
+  }
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then((registration) => {
+        registration.unregister();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+}
